f0fcb91703cc4a068b6e91bbfcbde3d0a075e54f
Ade Attwood <adrian.attwood@marketdojo.com>
2022-12-27 18:00:01 +0000

    docs: add comments to rust.lua
    This is to better explain what this thing dose now its linked to in the
    readme as examples.

--- lua/rust.lua
+++ lua/rust.lua
@@ -1,8 +1,17 @@
+-- Example configz module that will install and setup the rust toolchain
+
+--- Query the local DPKG database to see if a package is installed
+---
+---@param package string The package you would like to query
+---@return boolean
 local is_installed = function(package)
   local ok, output = configz.run("dpkg-query --show --showformat='${db:Status-Status}' " .. package)
   return ok and output == "installed"
 end
 
+--- Install a list of packages with APT if they are not already installed
+---
+---@param package_list string[] The list of packages
 local install_packages = function(package_list)
   local packages_to_install = {}
   for _, package in ipairs(package_list) do
@@ -17,8 +26,12 @@ local install_packages = function(package_list)
   end
 end
 
+-- Install the required packages, ca-certificates is required for
+-- configz.download and curl is required for rustup.sh
 install_packages { "ca-certificates", "curl" }
 
+-- Download and and run the rustup.sh script provided by rust to install and
+-- setup the rust toolchain
 local ok, path = configz.get_executable "rustc"
 if not ok then
   configz.download("/tmp/rustup.sh", { url = "https://sh.rustup.rs" })
