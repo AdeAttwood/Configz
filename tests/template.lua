0fad80a1919538f9b1a2e4dcc67cbcba92e7d34a
Ade Attwood <adrian.attwood@marketdojo.com>
2022-12-29 14:41:29 +0000

    feat: add template function to the standard library
    You can now create a file from a template and render dynamic templates
    with the liquid templating engin. This adds an extra function called
    `template` and will take a `source` and optional `data` to render the
    template.
    
    ```lua
    configz.template("/tmp/config", { source = "files/config" })
    ```
    
    This will render the template with no data and save the file to
    `/tmp/config`. It also supports table data of strings lists and nested
    tables.
    
    ```lua
    configz.template("/tmp/config", {
      source = "files/config",
      data = {
        string = "String",
        boolean = boolean,
        nested = { table = "Nested table" },
        list = { "One", "Two", "Three" }
      },
    })
    ```
    
    Ref: #4

---
+++ tests/template.lua
@@ -0,0 +1,43 @@
+local function test_template(dest, config)
+  local ok = configz.template(dest, config)
+  if not ok then
+    error(string.format([[file("%s") should have returned true]], dest))
+  end
+
+  if not configz.is_file "/tmp/configz/template.txt" then
+    error(string.format([["%s" is not a file]], dest))
+  end
+end
+
+local function read_file(file_path)
+  local file = io.open(file_path, "rb")
+  if file == nil then
+    return false, ""
+  end
+
+  local content = file:read "*all"
+  file:close()
+
+  return true, content
+end
+
+test_template("/tmp/configz/template.txt", { source = "tests/template.liquid" })
+test_template("/tmp/configz/template.data.txt", {
+  source = "tests/template.data.liquid",
+  data = {
+    name = "World",
+    bool_one = false,
+    bool_two = true,
+    user = { name = "The user name" },
+    list = { "one", "two", "three" },
+  },
+})
+
+local file_ok, content = read_file "/tmp/configz/template.data.txt"
+
+assert(file_ok)
+assert(content:find "Hello World!")
+assert(not content:find "Bool one is true")
+assert(content:find "UserName: The user name")
+assert(content:find "UserNameLower: the user name")
+assert(content:find "List: one, two, three")
