702ce4ff805864901a80de02bee11a05546558f1
Ade Attwood <adrian.attwood@marketdojo.com>
2022-12-29 14:41:29 +0000

    feat: add template function to the standard library
    You can now create a file from a template and render dynamic templates
    with the liquid templating engin. This adds an extra function called
    `template` and will take a `source` and optional `data` to render the
    template.
    
    ```lua
    configz.template("/tmp/config", { source = "files/config" })
    ```
    
    This will render the template with no data and save the file to
    `/tmp/config`. It also supports table data of strings lists and nested
    tables.
    
    ```lua
    configz.template("/tmp/config", {
      source = "files/config",
      data = {
        string = "String",
        boolean = boolean,
        nested = { table = "Nested table" },
        list = { "One", "Two", "Three" }
      },
    })
    ```
    
    Ref: #4

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use env_logger::{Builder, Target};
 use log::{debug, error, info};
-use rlua::{Context, Error, Function, Lua, Table};
+use rlua::{Context, Error, Function, Lua, Table, Value};
 use std::fs;
 use std::io::Write;
 use std::os::unix;
@@ -18,6 +18,42 @@ struct Args {
 }
 
 struct Configz;
+
+fn to_liquid_object(input: Value) -> liquid::model::Value {
+    match input {
+        Value::Boolean(value) => liquid::model::Value::Scalar(liquid::model::ScalarCow::new(value)),
+        Value::Integer(value) => liquid::model::Value::Scalar(liquid::model::ScalarCow::new(value)),
+        Value::Number(value) => liquid::model::Value::Scalar(liquid::model::ScalarCow::new(value)),
+        Value::String(value) => liquid::model::Value::Scalar(liquid::model::ScalarCow::new(
+            String::from_utf8(value.as_bytes().to_vec()).unwrap(),
+        )),
+        Value::Table(value) => {
+            let is_array = match value.contains_key(1) {
+                Ok(result) => result,
+                Err(_) => false,
+            };
+
+            if is_array {
+                let mut list = Vec::new();
+                for pair in value.pairs::<String, Value>() {
+                    let (_, v) = pair.unwrap();
+                    list.push(to_liquid_object(v))
+                }
+
+                liquid::model::Value::Array(list)
+            } else {
+                let mut table = liquid::model::Object::new();
+                for pair in value.pairs::<String, Value>() {
+                    let (k, v) = pair.unwrap();
+                    table.insert(k.into(), to_liquid_object(v));
+                }
+
+                liquid::model::Value::Object(table)
+            }
+        }
+        _ => liquid::model::Value::Nil,
+    }
+}
 
 impl Configz {
     pub fn debug<'lua>(context: Context<'lua>) -> Function {
@@ -42,6 +78,79 @@ impl Configz {
         context
             .create_function(|_, message: String| {
                 error!("{}", message);
+                Ok(true)
+            })
+            .unwrap()
+    }
+
+    pub fn template<'lua>(context: Context<'lua>) -> Function {
+        context
+            .create_function(|_, (destination, config): (String, Table)| {
+                let template_file: String = match config.get("source") {
+                    Ok(content) => content,
+                    Err(_) => {
+                        error!("[{}] missing required parameter source", destination);
+                        return Ok(false);
+                    }
+                };
+
+                let liquid = match liquid::ParserBuilder::with_stdlib().build() {
+                    Ok(value) => value,
+                    Err(err) => {
+                        error!("[{}] unable to build tempalte engin, {}", destination, err);
+                        return Ok(false);
+                    }
+                };
+
+                let template_content = match fs::read_to_string(&template_file) {
+                    Ok(value) => value,
+                    Err(_) => {
+                        error!(
+                            "[{}] unable to read template file {}",
+                            destination, template_file
+                        );
+                        return Ok(false);
+                    }
+                };
+
+                let template = match liquid.parse(&template_content) {
+                    Ok(value) => value,
+                    Err(err) => {
+                        error!(
+                            "[{}] unable to parse tempalte {} {}",
+                            destination, template_file, err
+                        );
+                        return Ok(false);
+                    }
+                };
+
+                let mut globals = liquid::model::Object::new();
+                let user_data = config.get("data");
+                if user_data.is_ok() {
+                    globals.insert("data".into(), to_liquid_object(user_data.unwrap()));
+                }
+
+                let content = match template.render(&globals) {
+                    Ok(value) => value,
+                    Err(err) => {
+                        error!("[{}] unable to render template {}", destination, err);
+                        return Ok(false);
+                    }
+                };
+
+                let mut file = match fs::File::create(&destination) {
+                    Ok(file) => file,
+                    Err(_) => {
+                        error!("[{}] unable to create the the file", destination);
+                        return Ok(false);
+                    }
+                };
+
+                info!(
+                    "[{}] created from template, {}",
+                    destination, &template_file
+                );
+                file.write_all(&content.as_bytes()).unwrap();
                 Ok(true)
             })
             .unwrap()
@@ -241,6 +350,7 @@ fn main() {
         configz.set("file", Configz::file(lua_ctx)).unwrap();
         configz.set("link", Configz::link(lua_ctx)).unwrap();
         configz.set("download", Configz::download(lua_ctx)).unwrap();
+        configz.set("template", Configz::template(lua_ctx)).unwrap();
         configz
             .set("directory", Configz::directory(lua_ctx))
             .unwrap();
